d5da746dbc543f08cce8f2ed9a633b68d31cbdfc
hariprakash-sparkout <hariprakash@sparkouttech.com>
2024-02-17 10:25:21 +0530

    fix

--- extension/angular/src/environments/environment.prod.ts
+++ extension/angular/src/environments/environment.prod.ts
@@ -5,4 +5,5 @@ export const environment = {
     url: 'https://qgkhqqydyzaxeqyskhrq.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFna2hxcXlkeXpheGVxeXNraHJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQwMDM5NjEsImV4cCI6MjAxOTU3OTk2MX0.bGAMZPgN7ErUYd-mBrPgqBwSDyq0LM8an-8lH8xr5ts',
   },
+  oauthClientId:'4c51dd4c-9b93-4b80-a0b2-4d107b8e0a0a'
 };
